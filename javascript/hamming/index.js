73a53d15f35799138a6cee5385e3f879e170397b
Michelle Hey <michelle.hey@metova.com>
2018-06-12 13:41:25 -0500

    completed code challenge in javascript and python

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -1,7 +1,18 @@
-// Define your Hamming class here.
-// `npm test` to ensure the unit tests pass and
-// your code meets all of the conditions.
-// You may use ES6 or ES5 to solve.
+var Hamming = function() {};
 
+Hamming.prototype.compute = function(first_strand, second_strand) {
+  if (first_strand.length !== second_strand.length) {
+    throw 'DNA strands must be of equal length.';
+  }
+
+  var count = 0;
+
+  for (var i = 0; i < first_strand.length; i++) {
+    if (first_strand[i] !== second_strand[i]) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = Hamming;
--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,13 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+def compute(first_strand, second_strand):
+    hamming = 0
+
+    if len(first_strand) != len(second_strand):
+      raise ValueError()
+
+    for x, y in zip(first_strand, second_strand):
+        if x != y:
+            hamming +=1
+    return hamming
